aee8457a6d23f7ae5a3291d78606f3d08b79602a
Prithi <preiths97@gmail.com>
2024-08-14 16:49:20 +0200

    Add components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import WeatherContainer from './weatherContainer';
+
+function App() {
+  return (
+    <div className="app">
+        <WeatherContainer />
+    </div>
+  );
+}
+
+export default App;
